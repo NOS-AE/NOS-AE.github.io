d166a5664d323a440638782ea4b814b3c2071093
nosae <2475945868@qq.com>
2025-01-23 13:37:51 +0800

    style: replace glightbox with fancybox

--- _javascript/modules/components/img-popup.js
+++ _javascript/modules/components/img-popup.js
@@ -1,50 +1,31 @@
 /**
  * Set up image popup
- *
- * Dependencies: https://github.com/biati-digital/glightbox
  */
 
-const lightImages = '.popup:not(.dark)';
-const darkImages = '.popup:not(.light)';
-let selector = lightImages;
-
-function updateImages(current, reverse) {
-  if (selector === lightImages) {
-    selector = darkImages;
-  } else {
-    selector = lightImages;
-  }
-
-  if (reverse === null) {
-    reverse = GLightbox({ selector: `${selector}` });
-  }
-
-  [current, reverse] = [reverse, current];
-}
-
 export function imgPopup() {
-  if (document.querySelector('.popup') === null) {
+  if (document.querySelector('[data-fancybox="gallery"]') === null) {
+    console.log("No popup images");
     return;
   }
 
-  const hasDualImages = !(
-    document.querySelector('.popup.light') === null &&
-    document.querySelector('.popup.dark') === null
-  );
-
-  if (Theme.visualState === Theme.DARK) {
-    selector = darkImages;
-  }
-
-  let current = GLightbox({ selector: `${selector}` });
-
-  if (hasDualImages && Theme.switchable) {
-    let reverse = null;
-
-    window.addEventListener('message', (event) => {
-      if (event.source === window && event.data && event.data.id === Theme.ID) {
-        updateImages(current, reverse);
-      }
-    });
-  }
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    Thumbs: {
+      type: "modern",
+    },
+    Toolbar: {
+      absolute: true,
+      display: {
+        left: ["infobar"],
+        middle: [
+          "zoomIn",
+          "zoomOut",
+          "toggle1to1",
+          "slideshow",
+          "thumbs",
+          "close"
+        ],
+        right: [],
+      },
+    },
+  });
 }
